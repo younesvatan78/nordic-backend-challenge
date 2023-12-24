3c2d8867b2099f4542a574a1c21e391289060e5a
Younes <younesvatan78@gmail.com>
2023-12-24 11:29:10 +0330

    test case for invalid user id and none exist user id

--- tests/Feature/WalletServiceTest.php
+++ tests/Feature/WalletServiceTest.php
@@ -32,6 +32,34 @@ class WalletServiceTest extends TestCase
         $user = User::factory()->create(['id'=>2]);
         $refrenceId = $this->WService->addMoney($user->id, 1000);
         $balance = $this->WService->getBalance($user->id);
+
+        // result
+        $this->assertNotEmpty($refrenceId);
+        $this->assertEquals(1000,$balance);
+    }
+
+    public function test_Add_Negetive_amount_To_User_Wallet(){
+        $user = User::factory()->create(['id' => 3]);
+
+        $refrenceId = $this->WService->addMoney($user->id,-50);
+
+        $balance = $this->WService->getBalance($user->id);
+
+        // result 
+        $this->assertNotEmpty($refrenceId);
+        $this->assertEquals(-50,$balance);
+    }
+    // throw Exception
+    public function test_Add_Money_With_Invalid_UserID(){
+        $this->expectException(\Exception::class);
+        $this->WService->addMoney('user',300);
+    }
+    // throw Exception
+
+    public function test_Add_Money_with_NonExistUserID(){
+        $this->expectException(\Exception::class);
+        $this->WService->addMoney(2000,50);
     }
     
+
 }
