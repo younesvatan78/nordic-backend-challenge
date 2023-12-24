681e90423fb689c59d50b9310fe0fcbf019c8fb1
Younes <younesvatan78@gmail.com>
2023-12-24 11:54:52 +0330

    add money method add to wallet service

--- app/Services/WalletService.php
+++ app/Services/WalletService.php
@@ -5,11 +5,57 @@ use App\Models\User;
 use App\Models\Transaction;
 
 class WalletService {
-    
+        
+    /**
+     * getBalance
+     *
+     * @param  int $user_id
+     * @return int balance 
+     */
     public function getBalance($user_id){
         $user = User::findOrFail($user_id);
         $wallet = $user->wallet;
         
         return $wallet->balance;
     }
+
+
+        
+    /**
+     * addMoney
+     *
+     * @param  int $user_id
+     * @param  int $amount
+     * @return int refrence id 
+     */
+    public function addMoney($user_id, $amount)
+    {
+        if ($amount < 0) {
+            throw new \Exception('Cannot add a negative amount to the wallet.');
+        }
+
+        $user = User::findOrFail($user_id);
+        $transaction = new Transaction([
+            'user_id' => $user_id,
+            'amount' => $amount,
+            'reference_id' => $this->generateReferenceId()
+        ]);
+
+        $transaction->save();
+        $wallet = $user->wallet;
+        $wallet->balance += $amount;
+        $wallet->save();
+
+        return $transaction->reference_id;
+
+    }
+    
+
+
+
+
+
+
+
+
 }
