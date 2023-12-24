e90686eea80f403bb72c43786a1da9e5c7426b05
Younes <younesvatan78@gmail.com>
2023-12-24 14:07:43 +0330

    some type fixes

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 
-Route::get('api/wallet/balance/{user_id}', [WalletController::class, 'getBalance']);
-Route::post('api/wallet/add-money', [WalletController::class, 'addMoney']);
+Route::get('wallet/balance/{user_id}', [WalletController::class, 'getBalance']);
+
+
+Route::post('wallet/add-money', [WalletController::class, 'addMoney']);
