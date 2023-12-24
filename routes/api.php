335dcda86e92dacc7e39a815425032b9696c1c37
Younes <younesvatan78@gmail.com>
2023-12-24 12:58:53 +0330

    route for controller added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\WalletController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/wallet/{user_id}/balance', [WalletController::class, 'getBalance']);
+Route::post('/wallet/add-money', [WalletController::class, 'addMoney']);
